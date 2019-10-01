8fd973b9ebaae6aef27051aa1ab046171398f535
venkat <ramarao.devavarapu321@gmail.com>
2019-10-01 17:22:35 +0530

    f

--- Travel/src/com/vir/dao/AdminDao.java
+++ Travel/src/com/vir/dao/AdminDao.java
@@ -9,9 +9,8 @@ public interface AdminDao {
 	BusService getService(int serviceId);
 	List<BusService> getAllService();
 	boolean cancelService(int serviceId);
-	/*List<Booking> viewFeedback();*/
+	
 	int updateDetails(BusService b);
-	//BusService update(int serviceId);
 	String addService(BusService b);
 	
 }
